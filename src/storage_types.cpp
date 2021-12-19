e43f47e189861949f810b9c4084af6fc500df99f
nksiazek <81360236+nksiazek@users.noreply.github.com>
2021-12-19 21:31:31 +0100

    Update storage_types.cpp

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "../include/storage_types.hpp"
+
+bool IPackageStockpile::empty() const {
+    return false;
+}
+
+
+size_t IPackageStockpile::size() const {
+    return false;
+}
+
+
+void IPackageStockpile::push(Package&&) {
+}
+
+
+IPackageStockpile::~IPackageStockpile() {
+
+}
