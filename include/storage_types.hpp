22944daf79efce476bba9a2d7ee9d785558c7afd
nksiazek <81360236+nksiazek@users.noreply.github.com>
2021-12-19 21:39:43 +0100

    Update storage_types.hpp

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -16,17 +16,16 @@ enum class PackageQueueType{
 class IPackageStockpile{
 public:
     using const_iterator = std::list<Package>::const_iterator;
-    virtual bool empty() const;
-    virtual size_t size() const;
-    void push(Package&&);
-    ~IPackageStockpile();
+    virtual bool empty() const = 0;
+    virtual size_t size() const = 0;
+    virtual void push(Package&&) = 0;
+    virtual ~IPackageStockpile() = 0;
 };
 
 class IPackageQueue{
 public:
-
-private:
-
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() const = 0;
 };
 
 class PackageQueue{
@@ -42,4 +41,3 @@ private:
 
 
 #endif //NETSIM_STORAGE_TYPES_HPP
-
