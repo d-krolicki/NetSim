8260ca4830b200abdba67886d42d6a560b914ab8
Julia Krysiak <81360507+julkry@users.noreply.github.com>
2022-01-10 18:09:47 +0100

    wszyscy tak samo maja to

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -5,4 +5,22 @@
 #ifndef NETSIM_HELPERS_HPP
 #define NETSIM_HELPERS_HPP
 
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <functional>
+#include "types.hpp"
+
+//double get_random();
+double your_num();
+
+extern std::random_device rd;
+extern std::mt19937 rng;
+
+extern double default_probability_generator();
+
+extern ProbabilityGenerator probability_generator;
+
+
 #endif //NETSIM_HELPERS_HPP
