85b6a8b62b81536a08a3a7abc7a4c576992d45e2
Julia Krysiak <81360507+julkry@users.noreply.github.com>
2022-01-10 18:19:53 +0100

    helpery to by sie nam przydaly

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "../include/helpers.hpp"
+#include <cstdlib>
+#include <random>
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double default_probability_generator() {
+    return std::generate_canonical<double, 10>(rng);
+}
+
+std::function<double()> probability_generator = default_probability_generator;
